0517fd20f00f85582cfadb81695ebab4da4c6d61
Mubashir <165596982+Mubashirtaj@users.noreply.github.com>
2026-01-19 14:11:24 -0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/context/tanstackprovider";
 import { ThemeProvider } from "@/context/ThemeContext";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
